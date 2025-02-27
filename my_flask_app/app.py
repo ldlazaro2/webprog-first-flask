4ea76b3b3a763dcb00b507d1117dabbe98b72b96
ldlazaro2 <ldlazaro2@student.apc.edu.ph>
2025-02-27 03:02:37 +0000

    source code added

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 books = [
     {"id": 1, "title": "1984", "author": "George Orwell"},
@@ -41,4 +43,4 @@ def delete_book(book_id):
     return '', 204
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
